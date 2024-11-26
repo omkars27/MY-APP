d26a31b9e044d86fa5f136d1aad96d4f371b8e55
omkars27 <omkarshindeos27@gmail.com>
2024-11-26 16:40:14 +0530

    nothing changes

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -5,7 +5,7 @@ package com.mycompany.app;
  */
 public class App {
 
-    private static final String MESSAGE = "Hello World! this is om";
+    private static final String MESSAGE = "Hello World! this is omkar";
 
     public App() {}
 
